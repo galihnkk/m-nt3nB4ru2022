860a501976fa08f76c45037f82c9a6b22918a591
arkans <82564511+arkansys22@users.noreply.github.com>
2022-08-21 15:12:35 +0700

    up

--- application/views/fronts/vendors/harga.php
+++ application/views/fronts/vendors/harga.php
@@ -83,7 +83,7 @@
 											<div class="share clearfix">
 											   <div class="contact-line"><p><strong>Bagikan :</strong></p></div>
                                               <p></p><li class="color-fb"><a href="http://www.facebook.com/sharer.php?u=<?php echo base_url("harga-detail/$post_h->judul_seo ") ?>" onclick="window.open('http://www.facebook.com/sharer.php?u=<?php echo base_url("harga-detail/$post_h->judul_seo ")?>','newwindow','width=400,height=350');  return false;" title="Facebook" target="_blank" ><i class="fa fa-facebook" ></i>Facebook</a></li>
-                                              <li class="color-ig"><a href="whatsapp://send?text=Stop keraguan Kamu dan Pasangan! Cari tau <?php echo $user_bisnis->namabisnis ?> yang ada di Mantenbaru. Ada <?php echo $post_h->judul ?> hanya dengan mengklik <?php echo base_url("harga-detail/$post_h->judul_seo ") ?> keraguan kamu dan pasangan bisa terselesaikan."><i class="fa fa-whatsapp"></i>Whatsapp</a></li>
+                                              <li class="color-ig"><a href="whatsapp://send?text=Stop keraguan Kamu dan Pasangan! Cari tau <?php echo $user_bisnis->namabisnis ?> yang ada di Mantenbaru. Ada <?php echo $post_h->judul ?> dengan harga Rp<?php echo number_format($post_h->harga,0,',','.') ?> hanya dengan mengklik <?php echo base_url("harga-detail/$post_h->judul_seo ") ?> keraguan kamu dan pasangan bisa terselesaikan."><i class="fa fa-whatsapp"></i>Whatsapp</a></li>
 
                                             </div>
 					    </div>
@@ -133,7 +133,7 @@
 												    			     <br><br>
                     											   <div class="contact-line"><p><strong>Bagikan :</strong></p></div>
                                                                   <p></p><li class="color-fb"><a href="http://www.facebook.com/sharer.php?u=<?php echo base_url("harga-detail/$post_h->judul_seo ") ?>" onclick="window.open('http://www.facebook.com/sharer.php?u=<?php echo base_url("harga-detail/$post_h->judul_seo ")?>','newwindow','width=400,height=350');  return false;" title="Facebook" target="_blank" ><i class="fa fa-facebook" ></i>Facebook</a></li>
-                                                                  <li class="color-ig"><a href="whatsapp://send?text=Stop keraguan Kamu dan Pasangan! Cari tau <?php echo $user_bisnis->namabisnis ?> yang ada di Mantenbaru.com. Ada <?php echo $post_h->judul ?> hanya dengan mengklik <?php echo base_url("harga-detail/$post_h->judul_seo ") ?> keraguan kamu dan pasangan bisa terselesaikan."><i class="fa fa-whatsapp"></i>Whatsapp</a></li>
+                                                                  <li class="color-ig"><a href="whatsapp://send?text=Stop keraguan Kamu dan Pasangan! Cari tau <?php echo $user_bisnis->namabisnis ?> yang ada di Mantenbaru.com. Ada <?php echo $post_h->judul ?> dengan harga Rp<?php echo number_format($post_h->harga,0,',','.') ?>  hanya dengan mengklik <?php echo base_url("harga-detail/$post_h->judul_seo ") ?> keraguan kamu dan pasangan bisa terselesaikan."><i class="fa fa-whatsapp"></i>Whatsapp</a></li>
                                                                 </div>
 								 							</div>
 							 						</div>
