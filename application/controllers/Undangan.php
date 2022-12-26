69d226e3f6cefc5c0b9da63da60de97657f234ff
arkans <82564511+arkansys22@users.noreply.github.com>
2022-12-26 17:00:06 +0700

    up

---
+++ application/controllers/Undangan.php
@@ -0,0 +1,159 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+class Undangan extends CI_Controller {
+
+
+
+	public function syifa_ganjar()
+	{
+		$this->load->view('fronts/uol/reluve/index');
+	}
+  public function sri_indra()
+  {
+    $this->load->view('fronts/uol/reluve/sri_indra');
+  }
+	public function anjani_bruce()
+	{
+		$this->load->view('fronts/uol/reluve/anjani_bruce');
+	}
+  public function raisa_hamish()
+  {
+    $this->load->view('fronts/uol/reluve/theme_2');
+  }
+
+		public function annisa_dhawy2()
+	{
+		$this->load->view('fronts/uol/pink_rose/annisa_dhawy');
+	}
+	public function adila_afif()
+	{
+		$this->load->view('fronts/uol/pink_rose/index');
+	}
+	public function ii_basis()
+	{
+		$this->load->view('fronts/uol/pink_rose/ii_basis');
+	}
+		public function sarra_anel()
+	{
+		$this->load->view('fronts/uol/pink_rose/sarra_anel');
+	}
+		public function ugahari_maulana()
+	{
+		$this->load->view('fronts/uol/pink_rose/ugahari_maulana');
+	}
+		public function vio_wahyudi()
+	{
+		$this->load->view('fronts/uol/pink_rose/vio_wahyudi');
+	}
+		public function oki_sidik()
+	{
+		$this->load->view('fronts/uol/pink_rose/oki_sidik');
+	}
+		public function fanny_paul()
+	{
+		$this->load->view('fronts/uol/pink_rose/fanny_paul');
+	}
+
+
+	public function miftah_japar()
+	{
+		$this->load->view('fronts/uol/love_bird/index');
+	}
+	public function dira_fiqih()
+	{
+		$this->load->view('fronts/uol/love_bird/fiqih');
+	}
+	public function hani_yudha()
+	{
+		$this->load->view('fronts/uol/love_bird/hani_yudha');
+	}
+
+	public function ica_arie()
+	{
+		$this->load->view('fronts/uol/love_bird/ica_arie');
+	}
+
+	public function ica_arie2()
+	{
+		$this->load->view('fronts/uol/love_bird/ica_arie2');
+	}
+
+	public function yeni_yudis()
+	{
+		$this->load->view('fronts/uol/love_bird/yeni_yudis');
+	}
+
+
+
+
+		public function anjela_maical()
+	{
+		$this->load->view('fronts/uol/welove/index');
+	}
+
+
+
+  public function meta_idris()
+{
+  $this->load->view('fronts/uol/weddingbook/meta_idris');
+}
+  public function arwati_wisnu()
+{
+  $this->load->view('fronts/uol/weddingbook/arwati_wisnu');
+}
+		public function ulfah_joe()
+	{
+		$this->load->view('fronts/uol/weddingbook/ulfah_joe');
+	}
+		public function ben_cahya()
+	{
+		$this->load->view('fronts/uol/weddingbook/ben_cahya');
+	}
+  public function kety_brandon()
+{
+  $this->load->view('fronts/uol/weddingbook/kety_brandon');
+}
+  public function peppy_djoko()
+{
+  $this->load->view('fronts/uol/weddingbook/wiliya_djoko');
+}
+  public function ayu_prasetya()
+{
+  $this->load->view('fronts/uol/weddingbook/ayu_prasetya');
+}
+    public function evie_roni()
+{
+  $this->load->view('fronts/uol/weddingbook/evie_roni');
+}
+      public function ilma_adit()
+{
+  $this->load->view('fronts/uol/weddingbook/ilma_adit');
+}
+  public function nety_andra()
+{
+  $this->load->view('fronts/uol/weddingbook/nety_andra');
+}
+
+
+	public function yuli_bayu()
+	{
+		$this->load->view('fronts/uol/green_scroll/yuli_bayu');
+	}
+
+
+
+			public function jane_nick()
+	{
+		$this->load->view('fronts/uol/weddingparadise/jane_nick');
+	}
+			public function lilis_femi()
+	{
+		$this->load->view('fronts/uol/weddingparadise/lilis_femi');
+	}
+			public function lia_yusuf()
+	{
+		$this->load->view('fronts/uol/weddingparadise/lia_yusuf');
+	}
+
+
+	}
