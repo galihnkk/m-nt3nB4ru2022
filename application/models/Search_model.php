a8268a2f6d506a021d734161ad564c1cfb142aff
Kevin Kamaluddin <kevin.kamaluddin@btn.co.id>
2025-01-01 16:30:02 +0700

    feat(search result): add lokasi

--- application/models/Search_model.php
+++ application/models/Search_model.php
@@ -7,10 +7,11 @@ class Search_model extends CI_Model {
         }
         $keywords = explode(' ', $query);
         $this->db->distinct();
-        $this->db->select('h.*, uc.user_company_judul');
+        $this->db->select('h.*, uc.user_company_judul, kab.nama');
         $this->db->from('harga h');
         $this->db->join('user_bisnis ub','h.harga_id_bisnis = ub.id_bisnis','inner');
         $this->db->join('user_company uc','ub.user_company_account = uc.user_company_id','inner');
+        $this->db->join('kabupaten kab','ub.kabupaten = kab.id','inner');
         $this->db->group_start();
         foreach ($keywords as $word) {
             $this->db->or_like('h.judul', $word);
